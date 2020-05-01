7f9077f0ab5d3e8ab50a8190129edd7b266a2d40
Dylan Maguire <dm4g17@soton.ac.uk>
2020-05-01 16:17:52 +0100

    Int eval

--- CEKMachine.hs
+++ CEKMachine.hs
@@ -12,9 +12,14 @@ data Frames = Done | AddLK Holes Exp | AddRK Exp Holes
 
 
 -- Add definition
+step (Int e) env _ = (Int e) --not sure what should go where the _ is placed
+
+
 step (Add exp1 exp2, env,kont) = step(exp1,env,AddLK HoleL exp2:kont)
 step (Int a,env,AddLK HoleL exp2:kont) = step (exp2, env,AddRK (Int a) HoleR:kont)
 step (Int b,env,AddRK (Int a) HoleR:kont) = step(Int (a + b),env,kont)
+
+
 
 
 -- Entry point for execution
