34249dca816b5c2932365acf6962b3fde2a745d2
Dylan Maguire <dm4g17@soton.ac.uk>
2020-05-01 16:42:54 +0100

    Primitive eval templates

--- CEKMachine.hs
+++ CEKMachine.hs
@@ -11,13 +11,55 @@ data Holes = HoleL | HoleR deriving Show
 data Frames = Done | AddLK Holes Exp | AddRK Exp Holes
 
 
--- Add definition
+--Int expression evaluation
 step (Int e) env _ = (Int e) --not sure what should go where the _ is placed
 
 
-step (Add exp1 exp2, env,kont) = step(exp1,env,AddLK HoleL exp2:kont)
-step (Int a,env,AddLK HoleL exp2:kont) = step (exp2, env,AddRK (Int a) HoleR:kont)
-step (Int b,env,AddRK (Int a) HoleR:kont) = step(Int (a + b),env,kont)
+step (Add exp1 exp2, env, kont) = step (exp1, env, AddLK HoleL exp2:kont)
+step (Int a, env, AddLK HoleL exp2:kont) = step (exp2, env, AddRK (Int a) HoleR:kont)
+step (Int b, env, AddRK (Int a) HoleR:kont) = step(Int (a + b), env, kont)
+
+step (Minus exp1 exp2, env, kont) = step (exp1, env, AddLK HoleL exp2:kont)
+step (Int a, env, AddLK HoleL exp2:kont) = step (exp2, env, AddRK (Int a) HoleR:kont)
+step (Int b, env, AddRK (Int a) HoleR:kont) = step(Int (a - b), env, kont)
+
+step (Multiply exp1 exp2, env, kont) = step (exp1, env, AddLK HoleL exp2:kont)
+
+
+
+
+step (Divide exp1 exp2, env, kont) = step (exp1, env, AddLK HoleL exp2:kont)
+
+
+
+
+step (Exponential exp1, env, kont) = step (exp1, env, AddLK HoleL exp2:kont)
+
+
+
+step (Negate exp1, env, kont) = step (exp1, env, AddLK HoleL exp2:kont)
+
+
+--Bool expression evaluation
+step (EBool 
+
+step (Equivalent 
+
+step (Not
+
+step (And
+
+step (Lesser
+
+step (Greater
+
+step (LesserEqual
+
+step (GreaterEqual
+
+step (Or
+
+
 
 
 
